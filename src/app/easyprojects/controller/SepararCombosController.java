dd9646ddf0cc5ae1c437d361a0f8e03cf860490f
fmontiel2019145 <fmontiel-2019145@kinal.edu.gt>
2020-08-18 23:57:03 -0600

    Auto stash before rebase of "refs/heads/cambios"

---
+++ src/app/easyprojects/controller/SepararCombosController.java
@@ -0,0 +1,45 @@
+package app.easyprojects.controller;
+
+import app.easyprojects.api.Files;
+import app.easyprojects.system.Principal;
+import java.io.FileNotFoundException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import java.util.Scanner;
+import javafx.fxml.Initializable;
+
+public class SepararCombosController implements Initializable{
+
+    private Principal principal = new Principal();
+    private Files file = new Files();
+    private Scanner entrada;
+    private Principal escenarioPrincipal;
+    
+    public void getFile() throws FileNotFoundException{
+        this.entrada = this.file.readFile();
+        
+        while(entrada.hasNext()){
+            
+        }
+    }
+    
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+    }
+    
+    public Principal getEscenarioPrincipal() {
+        return escenarioPrincipal;
+    }
+
+    public void setEscenarioPrincipal(Principal escenarioPrincipal) {
+        this.escenarioPrincipal = escenarioPrincipal;
+    }
+    
+    public void ventanaSepararCombos(){
+        escenarioPrincipal.ventanaSepararCombos();
+    }
+    
+    public void menuPrincipal(){
+        escenarioPrincipal.menuPrincipal();
+    }
+}
