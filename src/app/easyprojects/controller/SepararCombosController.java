78945186a714d4a68584ead9474b8b4d43a00fdb
fmontiel2019145 <fmontiel-2019145@kinal.edu.gt>
2020-08-19 01:15:27 -0600

    Combo separador al 100

--- src/app/easyprojects/controller/SepararCombosController.java
+++ src/app/easyprojects/controller/SepararCombosController.java
@@ -1,26 +1,70 @@
 package app.easyprojects.controller;
 
-import app.easyprojects.api.Files;
 import app.easyprojects.system.Principal;
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import java.util.Scanner;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javax.swing.JFileChooser;
 
 public class SepararCombosController implements Initializable{
 
-    private Principal principal = new Principal();
-    private Files file = new Files();
-    private Scanner entrada;
+    private final Principal principal = new Principal();
     private Principal escenarioPrincipal;
     
+    private JFileChooser selectorArchivo;
+    private File archivo;
+    private Scanner entrada;
+    private String ruta;
+    
+    private String emails = "";
+    private String claves = "";
+    
+    private String combo;
+    private String[] comboArray;
+    
+    private int i;
+    
+    @FXML private TextArea txtAreaEmails;
+    @FXML private TextArea txtAreaClaves;
+    @FXML private TextField txtDelimitador;
+    
     public void getFile() throws FileNotFoundException{
-        this.entrada = this.file.readFile();
+        this.selectorArchivo = new JFileChooser();
+        this.selectorArchivo.showOpenDialog(this.selectorArchivo);
+        this.ruta = this.selectorArchivo.getSelectedFile().getAbsolutePath();
         
-        while(entrada.hasNext()){
+        this.archivo = new File(ruta);
+        
+        this.entrada = new Scanner(this.archivo);
+        
+        this.txtAreaEmails.setText("");
+        this.txtAreaClaves.setText("");
+        
+        this.i = 0;
+        
+        while(this.entrada.hasNext()){
+            this.combo = this.entrada.nextLine();
+            this.comboArray = this.combo.split(this.txtDelimitador.getText());
             
+            if(this.comboArray[0] != null && this.comboArray[1] != null){
+                this.emails += this.comboArray[0]+"\n";
+                this.claves += this.comboArray[1]+"\n";
+            }else{
+                this.emails += "\n";
+                this.emails += "\n";
+            }
+            
+            this.i +=1;
         }
+        
+        this.txtAreaEmails.setText(this.emails);
+        this.txtAreaClaves.setText(this.claves);
     }
     
     @Override
---
+++ src/app/easyprojects/system/Principal.java
@@ -0,0 +1,74 @@
+package app.easyprojects.system;
+
+import app.easyprojects.controller.MenuPrincipalController;
+import app.easyprojects.controller.SepararCombosController;
+import java.io.InputStream;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.fxml.JavaFXBuilderFactory;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author DELL
+ */
+public class Principal extends Application {
+    private Stage escenarioPrincipal;
+    private Scene escena; 
+    private final String PAQUETE_VISTA = "/app/easyprojects/view/";
+    
+    @Override
+    public void start(Stage escenarioPrincipal) throws Exception {
+        this.escenarioPrincipal = escenarioPrincipal;
+        this.escenarioPrincipal.setTitle("CrackingTools - EasyProjects");
+        escenarioPrincipal.getIcons().add(new Image("/app/easyprojects/image/icono.png"));
+        //Parent root = FXMLLoader.load(getClass().getResource("/org/noelaguirre/view/MenuPrincipalView.fxml"));
+        //Scene escena = new Scene(root);
+        //escenarioPrincipal.setScene(escena);
+        menuPrincipal();
+        escenarioPrincipal.show();
+    }
+    
+    public void menuPrincipal(){
+        try{
+            MenuPrincipalController menuPrincipal = (MenuPrincipalController)cambiarEscena("MenuPrincipalView.fxml",600,406);
+            menuPrincipal.setEscenarioPrincipal(this);
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+    
+    public void ventanaSepararCombos(){
+        try{
+            SepararCombosController sc = (SepararCombosController)cambiarEscena("SepararCombosView.fxml",876,458);
+            sc.setEscenarioPrincipal(this);
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+    public Initializable cambiarEscena(String fxml, int ancho, int alto) throws Exception{
+        Initializable resultado = null;
+        FXMLLoader cargadorFXML = new FXMLLoader();
+        InputStream archivo = Principal.class.getResourceAsStream(PAQUETE_VISTA + fxml);
+        cargadorFXML.setBuilderFactory(new JavaFXBuilderFactory());
+        cargadorFXML.setLocation(Principal.class.getResource(PAQUETE_VISTA + fxml));
+        escena = new Scene((AnchorPane)cargadorFXML.load(archivo),ancho,alto);
+        escenarioPrincipal.setScene(escena);
+        escenarioPrincipal.sizeToScene();
+        resultado = (Initializable)cargadorFXML.getController();
+        
+        return resultado;
+    }
+}
